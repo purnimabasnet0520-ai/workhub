0b567cc2b7d05f7a825a98ea0bfaf80923abd724
purnimabasnet0520-ai <purnimabasnet0520@gmail.com>
2026-04-18 15:04:31 +0545

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf.urls.static import static
+from django.views.static import serve
 from main import settings
 
-from main import settings
 from .views import *
 
 urlpatterns = [
@@ -35,5 +35,7 @@ urlpatterns = [
     path('notifications/', include('notifications.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
