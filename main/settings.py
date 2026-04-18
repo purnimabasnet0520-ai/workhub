fdc4a098dd298ee9e09986f55d0b524c0499bda8
purnimabasnet0520-ai <purnimabasnet0520@gmail.com>
2026-04-18 21:39:22 +0545

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -194,6 +194,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_TIMEOUT = 20  # Timeout in seconds for SMTP connection
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "WORKHUB <noreply@workhub.com>")
